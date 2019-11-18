f1008b0ebd17a70fe50ba4415b6389db253e231a
edfranco <edfranco415@gmail.com>
2019-11-17 22:16:14 -0800

    created new project layout

--- src/containers/PortfolioContainer/PortfolioContainer.js
+++ src/containers/PortfolioContainer/PortfolioContainer.js
@@ -11,27 +11,40 @@ class PortfolioContainer extends Component {
         project: PROJECTS[0],
     };
 
-
+    projectImgs = () => {
+        const images = [];
+        this.state.projects.forEach(project => {
+            images.push(
+                <div className="project-container div-headers">
+                    <h2>{project.name}</h2>
+                    <img src={project.image} />
+                </div>
+            );
+        });
+        return images;
+    };
 
     render() {
-        console.log(this.state.projects);
         return (
             <div className="portfolio" id="portfolio">
                 <h2 className="div-headers">{this.state.project.name}</h2>
-                <img src={this.state.project.image} alt={this.state.project.name} />
-                <div className="project-buttons">
+                <div className="project-images">
+                    {this.projectImgs()}
+                </div>
+                {/* <img src={this.state.project.image} alt={this.state.project.name} /> */}
+                {/* <div className="project-buttons">
                     <button>Previous</button>
                     <button>Next</button>
-                </div>
-                <p>{this.state.project.content}</p>
-                <div className="project-buttons">
+                </div> */}
+                {/* <p>{this.state.project.content}</p> */}
+                {/* <div className="project-buttons">
                     <a href={this.state.project.deployed}>
                         <button>Live</button>
                     </a>
                     <a href={this.state.project.github}>
                         <button>Code</button>
                     </a>
-                </div>
+                </div> */}
 
             </div>
         );
